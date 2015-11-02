b146bcf79eb644884da85c01548f6dd4d14340da
prolic <saschaprolic@googlemail.com>
2015-11-02 20:51:13 +0000

    add license to all php files

--- src/TakeSnapshot.php
+++ src/TakeSnapshot.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of prooph/snapshotter.
+ * (c) 2014-2015 prooph software GmbH <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Date: 11/02/15 - 04:22 PM
+ */
 
 namespace Prooph\Snapshotter;
 
